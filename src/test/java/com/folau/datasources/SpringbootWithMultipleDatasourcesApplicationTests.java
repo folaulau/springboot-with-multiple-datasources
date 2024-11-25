c1a7e8c6d0c42d6aed2941da3fd88b9051c222c6
Folau Kaveinga <folaukaveinga@gmail.com>
2024-11-24 17:08:27 -0700

    setup

---
+++ src/test/java/com/folau/datasources/SpringbootWithMultipleDatasourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folau.datasources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithMultipleDatasourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
